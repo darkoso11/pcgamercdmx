bee2f23f2f1d05a2c61ad02ee18e013c2528d1ec
Oswald Garcia <86038906+darkoso11@users.noreply.github.com>
2025-07-02 22:03:17 -0600

    Agregando etiquetas "slug"

--- src/main.server.ts
+++ src/main.server.ts
@@ -2,6 +2,18 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
 
+export function getPrerenderParams() {
+  return [
+    // Productos
+    { route: '/productos/pc-gamer-intel-i7' },
+    { route: '/productos/pc-gamer-ryzen-5' },
+    // Blogs
+    { route: '/blog/como-armar-tu-pc' },
+    { route: '/blog/guia-componentes-2024' }
+    // Agrega aquí todos los slugs que quieras prerenderizar
+  ];
+}
+
 const bootstrap = () => bootstrapApplication(App, config);
 
 export default bootstrap;
