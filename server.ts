6747eba2bcd3568887a5bb44f3f4cee6aff70a6f
Oswald Garcia <86038906+darkoso11@users.noreply.github.com>
2025-07-02 21:13:58 -0600

    Solucionando otro error de vercel

--- server.ts
+++ server.ts
@@ -2,9 +2,9 @@
 
 export function getPrerenderParams() {
   return [
-    { slug: 'producto-1' },
-    { slug: 'producto-2' },
-    // Agrega aquí todos los slugs que quieras prerenderizar
+    { slug: 'ejemplo-producto-1' },
+    { slug: 'ejemplo-producto-2' },
+    // Agrega aquí todos los slugs de productos y blogs que quieras prerenderizar
   ];
 }
 
