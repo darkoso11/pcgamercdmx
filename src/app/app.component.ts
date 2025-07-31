8eff0884f86388184a0aedaabd5f36714ead34de
Oswald Garcia <86038906+darkoso11@users.noreply.github.com>
2025-07-30 22:02:55 -0600

    Terminando el home componet
    Ya estan todas la secciones que deben de ir en el home, pero se tiene problemas con la importacion del comonente del footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pcgamercdmx';
+  title = 'PC Gamer CDMX';
 }
-    NavbarComponent
--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  imports: [CommonModule, RouterModule],
+  templateUrl: './footer.component.html'
 })
 export class FooterComponent {
-  year = new Date().getFullYear();
+  year: number = new Date().getFullYear();
 }
+
