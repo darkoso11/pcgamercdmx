c9baff0a0a803f05040a0a0fc72c33733fc6e90b
Oswald Garcia <86038906+darkoso11@users.noreply.github.com>
2025-11-14 18:18:26 -0600

    Pequeña Actualizacion de twilwind
    Y un cambio pequeño en el blog

--- src/app/features/blog/blog.component.ts
+++ src/app/features/blog/blog.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-blog',
   standalone: true,
-  template: `<div class="text-white text-3xl p-8">Blog próximamente...</div>`,
+  template: `<div class="text-green-800 text-3xl p-8">Blog próximamente...</div>`,
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent {}
