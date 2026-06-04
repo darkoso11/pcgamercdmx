6afcc152b951e93e62996552846a348c6a8aecbd
Lynx Pardelle <lnxdrk@gmail.com>
2026-06-03 19:26:50 -0600

    Hide footer on admin routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 @Component({
@@ -8,5 +9,26 @@ import { FooterComponent } from './shared/components/footer/footer.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  showFooter = signal(true);
+  private sub?: Subscription;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.updateFooterVisibility(this.router.url);
+    this.sub = this.router.events.subscribe((ev) => {
+      if (ev instanceof NavigationEnd) {
+        this.updateFooterVisibility(ev.urlAfterRedirects);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub?.unsubscribe();
+  }
+
+  private updateFooterVisibility(url: string) {
+    this.showFooter.set(!url.startsWith('/admin'));
+  }
 }
